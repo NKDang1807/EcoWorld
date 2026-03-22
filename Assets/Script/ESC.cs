54511ccf925d73dbbe6c68215f7136091673fdf7
docao040606-ux <docao040606@gmail.com>
2026-03-22 21:01:06 +0700

    ...

--- Assets/Script/ESC.cs
+++ Assets/Script/ESC.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using Fusion; // Phải có thư viện mạng để đọc NetworkArray
+using Fusion.Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class ESC : MonoBehaviour 
 {
@@ -35,5 +37,17 @@ public class ESC : MonoBehaviour
         {
             Debug.Log("Đang mở bảng lên! Chạy code vẽ item trong Balo ra đây...");
         }
+    }
+    public async void ThoatGame()
+    {
+        // 1. Tìm anh giao hàng (Runner) đang chạy trong Map
+        NetworkRunner runner = FindObjectOfType<NetworkRunner>();
+        
+        if (runner != null)
+        {
+            await runner.Shutdown();
+            Destroy(runner.gameObject); 
+        }
+        SceneManager.LoadScene(0);
     }
 }
